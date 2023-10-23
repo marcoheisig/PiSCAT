3500d94c00c9dd9f55f81277b02959ecb4bb388b
Marco Heisig <marco.heisig@fau.de>
2023-10-23 20:30:45 +0200

    Automatically derive number of frames when reading RAW files.

--- piscat/video/from_raw_file.py
+++ piscat/video/from_raw_file.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import os.path
+
 import dask
 import dask.array as da
 import numpy as np
@@ -15,19 +17,26 @@ class Video_from_raw_file(Video_from_array):
     def from_raw_file(
         cls,
         filename: Filename,
-        shape: tuple[int, int, int],
+        shape: tuple[int, int, int] | tuple[int, int],
         dtype: npt.DTypeLike,
     ) -> Self:
         path = path_to_existing_file(filename)
+        nbytes = os.path.getsize(path)
         dtype = np.dtype(dtype)
-        (f, h, w) = shape
+        if len(shape) == 3:
+            (f, h, w) = shape
+        elif len(shape) == 2:
+            (h, w) = shape
+            f = nbytes // (h * w * dtype.itemsize)
+        else:
+            raise ValueError(f"Invalid raw video shape: {shape}")
         bytes_per_frame = h * w * dtype.itemsize
         bytes_per_chunk = 1024 * 1024 * 128
         frames_per_chunk = max(1, bytes_per_chunk // bytes_per_frame)
         load = dask.delayed(_chunk_from_raw_file)
         chunks = []
         for position in range(0, f, frames_per_chunk):
-            chunk_size = min(frames_per_chunk, shape[0] - position)
+            chunk_size = min(frames_per_chunk, f - position)
             offset = position * h * w * dtype.itemsize
             shape = (chunk_size, h, w)
             chunk = dask.array.from_delayed(
